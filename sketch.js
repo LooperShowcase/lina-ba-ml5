b5ab5b3ffd0a597684fcf6781628e04bee138873
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2021-07-22 09:13:53 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+let player;
+let bgImage;
+let playerImage;
+let obstacleImage;
+let obstacles = [];
+let wordClassifier;
+
+function preload() {
+  bgImage = loadImage("background.jpg");
+  playerImage = loadImage("player.png");
+  obstacleImage = loadImage("obstacle.png");
+  let options = {
+    probilityThreshold: 0.85,
+  };
+  wordClassifier = ml5.soundClassifier("SpeedCommands18w", options);
+}
+function setup() {
+  createCanvas(1000, 400);
+  player = new Player();
+  wordClassifier.classify(heardWord);
+}
+function heardWord(errors, results) {
+  let word = results[0].label;
+  if (word === "up") {
+    player.jump();
+  }
+  console.log(results[0].label, results[0].confidence);
+}
+function keyPressed() {
+  if (key === " ") {
+    player.jump();
+  }
+}
+
+function draw() {
+  if (random(1) < 0.01) {
+    obstacles.push(new Obstacle());
+  }
+
+  background(bgImage);
+
+  for (let obs of obstacles) {
+    obs.show();
+    obs.move();
+    if (player.collided(obs) === true) {
+      console.log("GAME OVER!");
+      noLoop();
+    }
+  }
+  player.show();
+
+  player.move();
+}
